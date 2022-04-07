e8df1640f144a3f4047c3764f7aca2fd880ca5b9
Dari Alves <darieltondf@gmail.com>
2022-04-07 00:20:26 -0300

    :tada: first commit

---
+++ php-crash/01_output.php
@@ -0,0 +1,32 @@
+<?php
+
+// echo - Output strings, numbers, html, etc
+// echo 123, 'php';
+
+// echo [1,2,3]; error
+
+// print - Works like echo, but can only take in a single argument
+// print 'php error';
+
+// print_r() - Print single values and arrays
+// print_r([1,2,3]);
+
+// var_dump() - Returns more info like data type and length
+// var_dump('php');
+
+// var_export() - similar to var_dump(). Outputs a string representation of a variable
+// var_export('php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php course</title>
+</head>
+<body>
+    <h1><?= 'Php'; ?> </h1>    
+</body>
+</html>
