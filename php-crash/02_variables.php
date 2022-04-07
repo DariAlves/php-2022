af484cb4ae5e5245f3e2b6f338f509a7d068b111
Dari Alves <darieltondf@gmail.com>
2022-04-07 00:34:32 -0300

    :sunglasses: variaveis e constantes

---
+++ php-crash/02_variables.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = 'Dari';
+$age = 36;
+$has_pet = true;
+
+// var_dump(($name));
+// var_dump(($age));
+// var_dump(($has_pet));
+
+// echo 'Nome: ' . $name; // Concatenação
+
+// echo "Nome: $name"; // Interpolação ou
+
+// echo "Nome: ${name}"; // Interpolação
+
+// $x = 10 + 10;
+
+// var_dump($x);
+
+
+// Constantes
+define('HOST', 'localhost');
+
+echo HOST;
